15843a10ad880c7a4b1ffabe7d2bd6dec202d2e4
Benj <benj>
2023-01-02 13:19:42 +0000

    Add basic blog layout

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const PATH = window.location.hostname.includes("github.io") ? "/blog" : "";
 
 /**
- * Retrieves a single Blog Post.
+ * Retrieves a single Blog Post metadata.
  * @param id Numeric identifier of the Blog Post.
- * @returns Blog Post content.
+ * @returns Blog Post metadata.
  */
-async function retrieveBlogPost(id) {
+async function retrieveBlogPostMetadata(id) {
+  if (!id) {
+    throw new Error(`No id specified`);
+  }
+
   let response;
   try {
     response = await fetch(`${PATH}/posts/${id}/metadata.json`);
   } catch (error) {
-    throw new Error(`Failed to initiate retrieval of Blog Post '${id}': ${error}`);
+    throw new Error(`Failed to initiate retrieval of Blog Post '${id}' metadata: ${error}`);
   }
 
   if (response.status === 404) {
@@ -18,17 +22,52 @@ async function retrieveBlogPost(id) {
   }
 
   if (!response.ok) {
-    throw new Error(`Failed to retrieve Blog Post '${id}': HTTP status code ${response.status}`);
+    throw new Error(`Failed to retrieve Blog Post '${id}' metadata: HTTP status code ${response.status}`);
   }
 
   let json;
   try {
     json = await response.json();
   } catch (error) {
-    throw new Error(`Failed to parse Blog Post '${id}' as JSON: ${error}`);
+    throw new Error(`Failed to parse Blog Post '${id}' metadata as JSON: ${error}`);
   }
 
   return { ...json, id };
+}
+
+/**
+ * Retrieves a single Blog Post content.
+ * @param id Numeric identifier of the Blog Post.
+ * @returns Blog Post content.
+ */
+async function retrieveBlogPostContent(id) {
+  if (!id) {
+    throw new Error(`No id specified`);
+  }
+
+  let response;
+  try {
+    response = await fetch(`${PATH}/posts/${id}/content.md`);
+  } catch (error) {
+    throw new Error(`Failed to initiate retrieval of Blog Post '${id}' content: ${error}`);
+  }
+
+  if (response.status === 404) {
+    return null;
+  }
+
+  if (!response.ok) {
+    throw new Error(`Failed to retrieve Blog Post '${id}' content: HTTP status code ${response.status}`);
+  }
+
+  let content;
+  try {
+    content = await response.text();
+  } catch (error) {
+    throw new Error(`Failed to parse Blog Post '${id}' content as text: ${error}`);
+  }
+
+  return content;
 }
 
 /**
@@ -41,7 +80,7 @@ async function retrieveAllBlogPostMetadatas() {
   let blogPosts = [];
 
   for (let startingBlogPostId = 1; startingBlogPostId < MAX_POSTS_EVER; startingBlogPostId += BATCH_SIZE) {
-    const results = await Promise.allSettled(getIdBatch(startingBlogPostId, BATCH_SIZE).map((id) => retrieveBlogPost(id)));
+    const results = await Promise.allSettled(getIdBatch(startingBlogPostId, BATCH_SIZE).map((id) => retrieveBlogPostMetadata(id)));
 
     const resultValues = results.filter((x) => x.status === "fulfilled").map((x) => x.value);
 
@@ -68,6 +107,52 @@ function getIdBatch(startingFromId, batchSize) {
 /**
  *
  */
+function appendBlogPostElement(blogPost) {
+  if (!blogPost) {
+    throw new Error(`Blog post empty`);
+  }
+
+  const blogPostContainer = document.querySelector("#blog-post-container");
+
+  const blogPostTemplate = document.querySelector("template#blog-post");
+  const blogPostTagTemplate = document.querySelector("template#blog-post-tag");
+
+  const blogPostElement = blogPostTemplate.content.cloneNode(true);
+
+  const titleElement = blogPostElement.querySelector(".blog-post-title");
+  titleElement.textContent = blogPost.title || "(Untitled)";
+
+  const timestampElement = blogPostElement.querySelector(".blog-post-timestamp");
+  const timestampDate = new Date(blogPost.timestamp);
+  timestampElement.textContent = `${timestampDate.toLocaleDateString(undefined, { month: "short" })} ${timestampDate.toLocaleDateString(undefined, {
+    day: "2-digit",
+  })}`;
+
+  const categoryElement = blogPostElement.querySelector(".blog-post-category");
+  categoryElement.textContent = blogPost.category || "General";
+
+  const contentElement = blogPostElement.querySelector(".blog-post-content");
+  contentElement.innerHTML = window.markdownit().render(blogPost.content || "");
+
+  const tagsElement = blogPostElement.querySelector(".blog-post-tags");
+
+  if (Array.isArray(blogPost.tags)) {
+    blogPost.tags.forEach((tag) => {
+      const blogPostTagElement = blogPostTagTemplate.content.cloneNode(true);
+
+      const nameElement = blogPostTagElement.querySelector(".blog-post-tag-name");
+      nameElement.textContent = tag;
+
+      tagsElement.appendChild(blogPostTagElement);
+    });
+  }
+
+  blogPostContainer.appendChild(blogPostElement);
+}
+
+/**
+ *
+ */
 function appendBlogPostSummaryElements(blogPostMetadatas) {
   const blogPostSummaryContainer = document.querySelector("#blog-post-summaries");
 
@@ -78,7 +163,10 @@ function appendBlogPostSummaryElements(blogPostMetadatas) {
     const blogPostElement = blogPostSummaryTemplate.content.cloneNode(true);
 
     const linkElement = blogPostElement.querySelector(".blog-post-summary-link");
-    linkElement.href = `${PATH}/posts/${blogPostMetadata.id}`;
+    linkElement.href = `${PATH}/posts?id=${blogPostMetadata.id}-${(blogPostMetadata.title || "")
+      .toLowerCase()
+      .replace(/\s+/g, "-")
+      .replace(/[^a-z0-9\-]/gi, "")}`;
 
     const titleElement = blogPostElement.querySelector(".blog-post-summary-title");
     titleElement.textContent = blogPostMetadata.title || "(Untitled)";
@@ -108,11 +196,3 @@ function appendBlogPostSummaryElements(blogPostMetadatas) {
     blogPostSummaryContainer.appendChild(blogPostElement);
   });
 }
-
-(async () => {
-  // Retrieve all Blog Posts
-  const blogPostMetadatas = await retrieveAllBlogPostMetadatas();
-
-  // Add each Blog Post to the page
-  appendBlogPostSummaryElements(blogPostMetadatas);
-})();
--- posts/1/content.md
+++ posts/1/content.md
@@ -1,1 +1,5 @@
-Hello world!
+New year, new me - I'm starting a blog!
+
+I've wanted to for a while, so here it goes.
+
+I'll blog about various topics, including software programming, cycling, and general life.
